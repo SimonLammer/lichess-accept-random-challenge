9559dc5a916d83a11224fe01170d3caee8f9fd14
Simon Lammer <lammer.simon@gmail.com>
2019-04-18 10:56:25 +0200

    Create webextension

---
+++ lichessARC.js
@@ -0,0 +1,36 @@
+function acceptChallenge() {
+  var challenges = document.getElementsByClassName("challenge");
+  var challenge = challenges[Math.floor(Math.random() * challenges.length)]
+  challenge.getElementsByClassName("accept")[0].click();
+}
+function init(container) {
+  var div = document.createElement("div");
+  div.innerText = "Accept random challenge";
+  div.id = "lichess-arc";
+  div.onclick = acceptChallenge;
+  container.prepend(div);
+}
+function loadContainer() {
+  e = document.getElementById('challenge_notifications_tag');
+  e.click(); // open challanges menu
+  e.click(); // close challanges menu
+}
+function initWhenContainerLoaded() {
+  var container = document.getElementById("challenge_app");
+  if (container.className.indexOf("rendered") > 0) {
+    init(container);
+  } else {
+    loadContainer();
+    setTimeout(initWhenContainerLoaded, 100);
+  }
+}
+
+oldOnload = window.onload;
+window.onload = function() {
+  if (typeof(oldOnload) == 'function') {
+    oldOnload();
+  }
+
+  console.log("onload");
+  initWhenContainerLoaded();
+}
--- script.js
+++
@@ -1,4 +0,0 @@
-$("#challenge_app").prepend('<div id="accept-random-challenge" style="padding:5px; border-radius:2px; background-color:#759900; color:#fff;">Accept random challenge</div>');
-$("#accept-random-challenge").click(function() {
-  $('.challenge').eq(Math.floor(Math.random() * $('.challenge').length)).find('.accept').click();
-});

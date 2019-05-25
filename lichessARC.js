bdf953cfbcdc29eb6de2a60ae8dbcfb60fce1688
Simon Lammer <lammer.simon@gmail.com>
2019-05-25 12:48:41 +0200

    add error messages

--- lichessARC.js
+++ lichessARC.js
@@ -1,3 +1,6 @@
+var CHALLENGES_CONTAINER_ID = 'challenge-app';
+var CHALLENGE_MENU_TOGGLE_ID = 'challenge-toggle';
+
 function acceptChallenge() {
   var challenges = document.getElementsByClassName("challenge");
   var challenge = challenges[Math.floor(Math.random() * challenges.length)]
@@ -11,12 +14,18 @@ function init(container) {
   container.prepend(div);
 }
 function loadContainer() {
-  e = document.getElementById('challenge-toggle');
+  e = document.getElementById(CHALLENGE_MENU_TOGGLE_ID);
+  if (!e) {
+    console.error("lichessARC could not find challenge menu toggle (element with id " + CHALLENGE_MENU_TOGGLE_ID + ")");
+  }
   e.click(); // open challanges menu
   e.click(); // close challanges menu
 }
 function initWhenContainerLoaded() {
-  var container = document.getElementById("challenge-app");
+  var container = document.getElementById(CHALLENGES_CONTAINER_ID);
+  if (!container) {
+    console.error("lichessARC could not find challenges container (element with id " + CHALLENGES_CONTAINER_ID + ")");
+  }
   if (container.className.indexOf("rendered") > 0) {
     init(container);
   } else {
